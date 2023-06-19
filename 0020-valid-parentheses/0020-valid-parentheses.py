04e27c2ec827442e77e4757a3162f0644db9876e
SaiGanesh21 <95938883+SaiGanesh21@users.noreply.github.com>
2023-06-19 14:19:12 +0530

    Time: 52 ms (27.37%), Space: 16.4 MB (44.76%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,89 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+        mapping={')':'(', '}':'{', ']':'['}
+        for x in s:
+            if x in mapping:
+                if stack and stack[-1]==mapping[x]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(x)
+        return not stack
+    
+        
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        stack=[]
+        mapping={')':'(','}':'{',']':'['}
+        for character in s:
+            if character in mapping:
+                if stack and stack[-1]==mapping[character]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(character)
+        return True if not stack else False
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        # stack = []
+        # for char in s:
+        #     if char == '(' or char == '{' or char == '[':
+        #         stack.append(char)
+        #     else:
+        #         if not stack:
+        #             return False
+        #         if char == ')' and stack[-1] == '(':
+        #             stack.pop()
+        #         elif char == '}' and stack[-1] == '{':
+        #             stack.pop()
+        #         elif char == ']' and stack[-1] == '[':
+        #             stack.pop()
+        #         else:
+        #             return False
+        # return not stack
