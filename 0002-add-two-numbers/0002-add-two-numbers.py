7df97c243bc39b2dc6cf39eae9cc64a73ee33352
SaiGanesh21 <95938883+SaiGanesh21@users.noreply.github.com>
2023-06-29 12:18:04 +0530

    Time: 82 ms (49.90%), Space: 16.4 MB (66.76%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,21 +5,45 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy=ListNode()
-        curr=dummy
-        
-        carry=0
+        returnlist=ListNode()
+        head=returnlist
+        carry=0 
         while l1 or l2 or carry:
-            v1=l1.val if l1 else 0
-            v2=l2.val if l2 else 0
+            cursum=0
+            if l1:
+                cursum+=l1.val
+                l1=l1.next
+            if l2:
+                cursum+=l2.val
+                l2=l2.next
+            cursum+=carry
+            digit=cursum%10
+            carry=cursum//10
+            returnlist.next=ListNode(digit)
+            returnlist=returnlist.next
+        return head.next
+            
+        
+        
+#         dummy=ListNode()
+#         curr=dummy
+        
+#         carry=0
+#         while l1 or l2 or carry:
+#             v1=l1.val if l1 else 0
+#             v2=l2.val if l2 else 0
             
             
-            val=v1+v2+carry
-            carry=val//10
-            val=val%10
-            curr.next=ListNode(val)
+#             val=v1+v2+carry
+#             carry=val//10
+#             val=val%10
+#             curr.next=ListNode(val)
             
-            curr=curr.next
-            l1=l1.next if l1 else None
-            l2=l2.next if l2 else None
-        return dummy.next
+#             curr=curr.next
+#             l1=l1.next if l1 else None
+#             l2=l2.next if l2 else None
+#         return dummy.next
+    
+    
+
+    
