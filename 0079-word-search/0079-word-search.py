b0dc12ff582d1c3d6960c140a336b911569fa641
SaiGanesh21 <95938883+SaiGanesh21@users.noreply.github.com>
2023-06-30 09:10:11 +0530

    Time: 7475 ms (58.12%), Space: 16.2 MB (96.87%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,27 @@
+class Solution:
+    def dfs(self,board,word,row,col,index):
+            if index>=len(word):
+                return True
+            if row>=len(board) or row<0 or col>=len(board[0]) or col<0  or board[row][col]!=word[index]:
+                return False
+            letter=board[row][col]
+            board[row][col]=""
+            found=self.dfs(board,word,row-1,col,index+1) or self.dfs(board,word,row+1,col,index+1) or self.dfs(board,word,row,col-1,index+1) or self.dfs(board,word,row,col+1,index+1)
+            board[row][col]=letter
+            return found
+    def exist(self, board: List[List[str]], word: str) -> bool:
+
+        rows=len(board)
+        cols=len(board[0])
+        for row in range(rows):
+            for col in range(cols):
+                if board[row][col]==word[0] and self.dfs(board,word,row,col,0):
+                    return True
+        return False
+                    
+                    
+
+       
+                    
+        
+        
