09d9b9b68a4532f9e3fb7ca830b67ecbdc37c54e
SaiGanesh21 <95938883+SaiGanesh21@users.noreply.github.com>
2023-07-05 15:20:39 +0530

    Time: 66 ms (10.06%), Space: 16.4 MB (7.34%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,8 +1,6 @@
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-    
 
-        
         index=-1
         n=len(nums)
         for i in range(n-2,-1,-1):
@@ -17,7 +15,7 @@ class Solution:
                 nums[i],nums[index]=nums[index],nums[i]
                 break
         nums[index+1:]=reversed(nums[index+1:])
-        return nums
+        
         
             
         
