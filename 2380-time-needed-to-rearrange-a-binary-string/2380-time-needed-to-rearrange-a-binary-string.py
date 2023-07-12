5656f9e7adeccf3e1fb155505f9378a9f1bd1bce
SaiGanesh21 <95938883+SaiGanesh21@users.noreply.github.com>
2023-07-12 07:24:51 +0530

    Time: 146 ms (47.15%), Space: 16.4 MB (19.92%) - LeetHub

---
+++ 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.py
@@ -0,0 +1,8 @@
+class Solution:
+    def secondsToRemoveOccurrences(self, s: str) -> int:
+        res=0
+        while "01" in s:
+            res+=1
+            s=s.replace("01","10")
+        return res
+  
