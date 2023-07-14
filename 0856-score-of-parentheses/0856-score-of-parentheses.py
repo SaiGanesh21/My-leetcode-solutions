3b1308c2b66de6b6a701a5db942b979027acff8c
SaiGanesh21 <95938883+SaiGanesh21@users.noreply.github.com>
2023-07-14 08:09:35 +0530

    Time: 55 ms (6.89%), Space: 16.3 MB (38.78%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.py
+++ 0856-score-of-parentheses/0856-score-of-parentheses.py
@@ -3,14 +3,14 @@ class Solution:
         stack=[]
         for x in s:
             if x =="(":
-                stack.append(-1)
+                stack.append(0)
             else:
-                if stack[-1]==-1:
+                if stack[-1]==0:
                     stack.pop()
                     stack.append(1)
                 else:
                     val=0
-                    while stack[-1]!=-1:
+                    while stack[-1]!=0:
                         val+=stack.pop()
                     stack.pop()
                     stack.append(2*val)
