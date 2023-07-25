5187d6775d350b4fcc19d7a5fd393f9c1f89ce01
SaiGanesh21 <95938883+SaiGanesh21@users.noreply.github.com>
2023-07-25 09:30:22 +0530

    Time: 607 ms (33.75%), Space: 30.3 MB (10.03%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -4,18 +4,18 @@ class Solution:
 
         # Edge cases:
 
-        if arr[0] > arr[1]:
-            return 0
-        if arr[n - 1] > arr[n - 2]:
-            return n - 1
+#         if arr[0] > arr[1]:
+#             return 0
+#         if arr[n - 1] > arr[n - 2]:
+#             return n - 1
 
         low = 1
         high = n - 2
-        while low <= high:
+        while low <=high:
             mid = (low + high) // 2
 
             # If arr[mid] is the peak:
-            if arr[mid - 1] < arr[mid] and arr[mid] > arr[mid + 1]:
+            if arr[mid - 1] < arr[mid] and mid-1>=0 and arr[mid] > arr[mid + 1] and mid+1<n:
                 return mid
 
             # If we are in the left:
