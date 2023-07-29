5eac3e05babea6e394610e237ec2e46687fb388a
SaiGanesh21 <95938883+SaiGanesh21@users.noreply.github.com>
2023-07-30 03:16:19 +0530

    Time: 494 ms (99.84%), Space: 29.6 MB (77.60%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -1,16 +1,27 @@
 class Solution:
     def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
-        a=sum(arr[0:k])
-        c=0
-        if a//k>=threshold:
-            c+=1
-    
-        for i in range(len(arr)-k):
-            a=a-arr[i]+arr[i+k]
-            if a//k>=threshold:
+        c,j,n=0,0,len(arr)
+        s=sum(arr[:k])
+        if s/k>=threshold:
+            c=1
+        for i in range(k,n):
+            s+=arr[i]-arr[j]
+            if s/k>=threshold:
                 c+=1
-        print(c)
+            j+=1
         return c
+
+#         a=sum(arr[0:k])
+#         c=0
+#         if a//k>=threshold:
+#             c+=1
+    
+#         for i in range(len(arr)-k):
+#             a=a-arr[i]+arr[i+k]
+#             if a//k>=threshold:
+#                 c+=1
+#         print(c)
+#         return c
     
 #     count = 0
 # current_sum = sum(arr[:k])  # 1-st window
