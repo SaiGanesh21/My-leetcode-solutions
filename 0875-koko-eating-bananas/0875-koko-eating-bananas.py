2307c9bf19258a357d5d0d99e7fef9b4069bcfff
SaiGanesh21 <95938883+SaiGanesh21@users.noreply.github.com>
2023-08-02 18:37:11 +0530

    Time: 304 ms (98.22%), Space: 17.9 MB (49.35%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -3,7 +3,63 @@ class Solution:
         l=1
         r=max(piles)
         
+        def time(m):
+            c=0
+            for x in piles:
+                c+=ceil(x/m)
+            return c<=h
+                
         while l<=r:
+            m=(l+r)//2
+            
+            if time(m):
+                r=m-1
+            else:
+                l=m+1
+        return l
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        l=1
+        r=max(piles)
+        
+        while l<r:
             mid=(l+r)//2
             totaltime=0
             for i in range(len(piles)):
