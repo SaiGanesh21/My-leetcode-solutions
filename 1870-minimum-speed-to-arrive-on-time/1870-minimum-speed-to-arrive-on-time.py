0d12ded8d4ac908a550897761959e3a4a3a69a63
SaiGanesh21 <95938883+SaiGanesh21@users.noreply.github.com>
2024-01-02 15:27:30 +0530

    Time: 2202 ms (61.36%), Space: 31.3 MB (26.70%) - LeetHub

---
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minSpeedOnTime(self, dist: List[int], hour: float) -> int:
+     
+        
+        def minpos(m):
+            c=0
+            n=len(dist)
+            for i in range(n-1):
+                c+=math.ceil(dist[i]/m)
+            c+=dist[n-1]/m
+            return c<=hour
+        
+        l=1
+        r=10**7
+        while l<=r:
+            m=(l+r)//2
+            if minpos(m):
+                r=m-1
+            else:
+                l=m+1
+        if minpos(l):
+            return l
+        return -1
