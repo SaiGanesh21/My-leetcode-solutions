5fa743e91ab918a19c0055b929f7450b7daab046
SaiGanesh21 <95938883+SaiGanesh21@users.noreply.github.com>
2024-01-13 12:50:42 +0530

    Time: 1086 ms (65.89%), Space: 17.8 MB (11.12%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -13,7 +13,7 @@ class Solution:
         
         for i in range(1,len(nums)):
             for j in range(0,i):
-                if nums[i]>nums[j] and lis[i]<lis[j]+1:
+                if nums[i]>nums[j] and lis[i]<=lis[j]+1:
                     lis[i]=lis[j]+1
         return max(lis)
 
