5cbc20d8a56d834fb6ec7046b049b42754151c48
SaiGanesh21 <95938883+SaiGanesh21@users.noreply.github.com>
2024-01-13 12:50:21 +0530

    Time: 1282 ms (64.55%), Space: 17.9 MB (11.12%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,9 +1,30 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        ans=[1]*len(nums)
+#         ans=[1]*len(nums)
         
-        for i in range(len(nums)):
+#         for i in range(len(nums)):
+#             for j in range(0,i):
+#                 if nums[j]<nums[i]:
+#                     ans[i]=max(ans[i],1+ans[j])
+#         return max(ans)
+    
+    
+        lis=[1]*len(nums)
+        
+        for i in range(1,len(nums)):
             for j in range(0,i):
-                if nums[j]<nums[i]:
-                    ans[i]=max(ans[i],1+ans[j])
-        return max(ans)
+                if nums[i]>nums[j] and lis[i]<lis[j]+1:
+                    lis[i]=lis[j]+1
+        return max(lis)
+
+# 	/* Compute optimized LIS values in bottom up manner */
+# 	for (int i = 1; i < n; i++ ) 
+# 	{ 
+# 		lis[i] = 1; 
+# 		for (int j = 0; j < i; j++ ) 
+# 			if ( arr[i] > arr[j] && lis[i] < lis[j] + 1) 
+# 				lis[i] = lis[j] + 1; 
+# 	} 
+
+# 	// Return maximum value in lis[] 
+# 	return *max_element(lis, lis+n); 
